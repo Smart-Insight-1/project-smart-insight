4a230a779b397ff28519314345be667c95c62281
carloslsilva01 <carlos.lsilva@sptech.school>
2026-04-20 19:08:39 -0300

    Atualizando Banco de Dados (v3.0) e Modelagem

--- banco-de-dados/BDsmartInsight.sql
+++ banco-de-dados/BDsmartInsight.sql
@@ -15,7 +15,6 @@ CREATE TABLE contato (
     id_contato INT AUTO_INCREMENT PRIMARY KEY,
     id_usuario INT NOT NULL,
     telefone CHAR(11),
-    celular CHAR(11),
     FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario)
 );
 
@@ -71,8 +70,8 @@ CREATE TABLE sensor (
 CREATE TABLE interacao (
     id_interacao INT AUTO_INCREMENT PRIMARY KEY,
     id_sensor INT NOT NULL,
-    tempo_inicio DATETIME NOT NULL,
-    tempo_fim DATETIME,
+    horario DATETIME,
+    duracao FLOAT,
     FOREIGN KEY (id_sensor) REFERENCES sensor(id_sensor)
 );
 
@@ -113,132 +112,23 @@ VALUES
 (3,'Essencial','Perfume Masculino','7892222222221','Natura'),
 (3,'Kaiak','Perfume Masculino','7892222222222','Natura');
 
+SELECT * FROM sensor;
+
 INSERT INTO sensor (id_setor,id_produto,data_instalacao)
 VALUES
-(1,1,'2026-03-01'),
-(1,2,'2026-03-01'),
-(2,3,'2026-03-01'),
+(1,1,'2026-03-01');
 
-(3,4,'2026-03-01'),
-(4,5,'2026-03-01'),
+SELECT * FROM interacao;
 
-(5,6,'2026-03-01'),
-(5,7,'2026-03-01');
-
-INSERT INTO interacao (id_sensor,tempo_inicio,tempo_fim)
+INSERT INTO interacao (id_sensor, horario, duracao)
 VALUES
-(1,'2026-03-30 14:02:10','2026-03-30 14:02:18'),
-(1,'2026-03-30 14:05:20','2026-03-30 14:05:27'),
-(2,'2026-03-30 14:10:10','2026-03-30 14:10:20'),
-(3,'2026-03-30 14:12:30','2026-03-30 14:12:42'),
-
-(4,'2026-03-30 15:02:10','2026-03-30 15:02:22'),
-(4,'2026-03-30 15:05:20','2026-03-30 15:05:35'),
-(5,'2026-03-30 15:07:30','2026-03-30 15:07:40'),
-
-(6,'2026-03-30 16:10:15','2026-03-30 16:10:28'),
-(7,'2026-03-30 16:12:00','2026-03-30 16:12:10'),
-
-(1,'2026-03-30 16:20:10','2026-03-30 16:20:18'),
-(2,'2026-03-30 16:21:05','2026-03-30 16:21:14'),
-(3,'2026-03-30 16:22:30','2026-03-30 16:22:40'),
-(1,'2026-03-30 16:23:12','2026-03-30 16:23:21'),
-(2,'2026-03-30 16:24:45','2026-03-30 16:24:55'),
-
-(4,'2026-03-30 16:25:10','2026-03-30 16:25:22'),
-(5,'2026-03-30 16:26:18','2026-03-30 16:26:29'),
-(4,'2026-03-30 16:27:05','2026-03-30 16:27:17'),
-(5,'2026-03-30 16:28:44','2026-03-30 16:28:53'),
-(4,'2026-03-30 16:29:30','2026-03-30 16:29:40'),
-
-(6,'2026-03-30 16:30:15','2026-03-30 16:30:25'),
-(7,'2026-03-30 16:31:10','2026-03-30 16:31:20'),
-(6,'2026-03-30 16:32:05','2026-03-30 16:32:16'),
-(7,'2026-03-30 16:33:30','2026-03-30 16:33:42'),
-(6,'2026-03-30 16:34:12','2026-03-30 16:34:23'),
-
-(1,'2026-03-30 17:01:15','2026-03-30 17:01:24'),
-(2,'2026-03-30 17:02:10','2026-03-30 17:02:20'),
-(3,'2026-03-30 17:03:40','2026-03-30 17:03:50'),
-(1,'2026-03-30 17:04:30','2026-03-30 17:04:39'),
-(2,'2026-03-30 17:05:18','2026-03-30 17:05:30'),
-
-(4,'2026-03-30 17:06:45','2026-03-30 17:06:56'),
-(5,'2026-03-30 17:07:25','2026-03-30 17:07:36'),
-(4,'2026-03-30 17:08:12','2026-03-30 17:08:22'),
-(5,'2026-03-30 17:09:03','2026-03-30 17:09:15'),
-
-(6,'2026-03-30 17:10:11','2026-03-30 17:10:23'),
-(7,'2026-03-30 17:11:40','2026-03-30 17:11:52'),
-(6,'2026-03-30 17:12:05','2026-03-30 17:12:16'),
-(7,'2026-03-30 17:13:30','2026-03-30 17:13:42'),
-(6,'2026-03-30 17:14:50','2026-03-30 17:15:01'),
-(7,'2026-03-30 17:15:25','2026-03-30 17:15:36');
-
-
--- SELECT COMPLETO 
-SELECT 
-l.id_loja,
-l.nome_loja,
-sa.nome_setor,
-p.nome AS produto,
-p.marca,
-s.id_sensor,
-i.id_interacao,
-i.tempo_inicio,
-i.tempo_fim,
-TIMESTAMPDIFF(SECOND, i.tempo_inicio, i.tempo_fim) AS tempo_interacao_segundos
-FROM interacao i
-JOIN sensor s 
-ON i.id_sensor = s.id_sensor
-JOIN produto p 
-ON s.id_produto = p.id_produto
-JOIN setor_amostra sa 
-ON s.id_setor = sa.id_setor
-JOIN loja l 
-ON sa.id_loja = l.id_loja
-ORDER BY i.tempo_inicio;
-
--- Interações por Produto
-SELECT 
-p.nome AS produto,
-COUNT(i.id_interacao) AS total_interacoes
-FROM interacao i
-JOIN sensor s ON i.id_sensor = s.id_sensor
-JOIN produto p ON s.id_produto = p.id_produto
-WHERE p.id_loja = 1
-GROUP BY p.nome
-ORDER BY total_interacoes DESC;
-
--- Setores e suas Interações
-SELECT 
-sa.nome_setor,
-COUNT(i.id_interacao) AS total_interacoes
-FROM interacao i
-JOIN sensor s ON i.id_sensor = s.id_sensor
-JOIN setor_amostra sa ON s.id_setor = sa.id_setor
-WHERE sa.id_loja = 1
-GROUP BY sa.nome_setor
-ORDER BY total_interacoes DESC;
-
--- Interações por Hora
-SELECT 
-HOUR(i.tempo_inicio) AS hora,
-COUNT(i.id_interacao) AS total_interacoes
-FROM interacao i
-JOIN sensor s ON i.id_sensor = s.id_sensor
-JOIN produto p ON s.id_produto = p.id_produto
-WHERE p.id_loja = 1
-GROUP BY hora
-ORDER BY hora;
-
--- Tempo Médio de Retenção por Produto
-SELECT 
-p.nome AS produto,
-AVG(TIMESTAMPDIFF(SECOND,i.tempo_inicio,i.tempo_fim)) AS tempo_medio_segundos
-FROM interacao i
-JOIN sensor s ON i.id_sensor = s.id_sensor
-JOIN produto p ON s.id_produto = p.id_produto
-WHERE p.id_loja = 1
-GROUP BY p.nome
-ORDER BY tempo_medio_segundos DESC;
+(1, '2026-04-20 10:05:12', 3.5),
+(1, '2026-04-20 10:17:48', 5.2),
+(1, '2026-04-20 10:32:10', 2.1),
+(1, '2026-04-20 11:03:27', 6.8),
+(1, '2026-04-20 11:25:09', 4.0),
+(1, '2026-04-20 12:10:44', 7.3),
+(1, '2026-04-20 13:55:18', 1.9),
+(1, '2026-04-20 14:22:36', 3.7),
+(1, '2026-04-20 15:40:02', 8.5),
+(1, '2026-04-20 16:05:50', 2.6);
