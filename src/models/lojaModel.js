1fd84fdadb0ea1592c75eb3b37bd41ca80b6ca19
carloslsilva01 <carlos.lsilva@sptech.school>
2026-05-24 00:20:39 -0300

    Salvando ID_EMPRESA sessionStorage e redirecionamento dashboard

--- src/models/lojaModel.js
+++ src/models/lojaModel.js
@@ -3,7 +3,7 @@ var database = require("../database/config")
 function buscarLojaUsuario(id) {
     console.log('acessando model loja');
 
-    var instrucaoSql = `SELECT al.id_loja, l.nome_loja FROM acesso_loja al
+    var instrucaoSql = `SELECT al.id_loja, l.nome_loja, l.id_empresa FROM acesso_loja al
     JOIN loja l ON al.id_loja = l.id_loja
     WHERE al.id_usuario = '${id}';`
 
