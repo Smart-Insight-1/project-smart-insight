dec337ac30819c183e53d1869a5a1c8f6d486a45
felipefalconi <felipefalconi9@gmail.com>
2026-06-04 23:50:22 -0300

    feat: adiciona controller grafico 03

--- src/controllers/dashboardController.js
+++ src/controllers/dashboardController.js
@@ -23,6 +23,19 @@ function obterInteracoesPorSetor(req, res) {
 }
 function obterTempoRetencao(req, res) {
     var id_loja = req.params.idLoja;
+
+    if (id_loja == undefined) {
+        res.status(400).send("O ID da loja está undefined!");
+    } else {
+        dashboardModel.buscarComparativo(id_loja)
+            .then(function (resultado) {
+                res.json(resultado);
+            })
+            .catch(function (erro) {
+                console.log(erro);
+                res.status(500).json(erro.sqlMessage);
+            });
+    }
 }
 function obterInteracoesPorHora(req, res) {
     var id_loja = req.params.idLoja;
