ee1591a1928996c2df0bc3452feab19063c04f1f
felipefalconi <felipefalconi9@gmail.com>
2026-06-07 17:30:26 -0300

    fix: readição de controller grafico 03

--- src/controllers/dashboardController.js
+++ src/controllers/dashboardController.js
@@ -4,11 +4,11 @@ function obterInteracoesPorProduto(req, res) {
     let id_loja = req.params.idLoja;
 
     dashboardModel.obterInteracoesPorProduto(id_loja)
-        .then(function(resultado) {
+        .then(function (resultado) {
             console.log(`\nResultados encontrados: ${resultado.length}`);
             console.log(`Resultados: ${JSON.stringify(resultado)}`);
             res.json(resultado);
-        }).catch(function(erro) {
+        }).catch(function (erro) {
             console.log(erro);
             res.status(500).json(erro.sqlMessage);
         });
@@ -18,17 +18,31 @@ function obterInteracoesPorSetor(req, res) {
     let id_loja = req.params.idLoja;
 
     dashboardModel.obterInteracoesPorSetor(id_loja)
-        .then(function(resultado) {
+        .then(function (resultado) {
             console.log(`\nResultados encontrados: ${resultado.length}`);
             console.log(`Resultados: ${JSON.stringify(resultado)}`);
             res.json(resultado);
-        }).catch(function(erro) {
+        }).catch(function (erro) {
             console.log(erro);
             res.status(500).json(erro.sqlMessage);
         });
 }
 
 function obterTempoRetencao(req, res) {
+    var id_loja = req.params.idLoja;
+
+    if (id_loja == undefined) {
+        res.status(400).send("O ID da loja está undefined!");
+    } else {
+        dashboardModel.obterTempoRetencao(id_loja)
+            .then(function (resultado) {
+                res.json(resultado);
+            })
+            .catch(function (erro) {
+                console.log(erro);
+                res.status(500).json(erro.sqlMessage);
+            });
+    }
 }
 
 function obterInteracoesPorHora(req, res) {
@@ -38,7 +52,7 @@ function obterKpis(req, res) {
     let id_loja = req.params.idLoja;
     let dataInicio = req.query.dataInicio;
     let dataFim = req.query.dataFim;
-    
+
 
     if (!dataInicio || !dataFim) {
         res.status(400).json("Informe as datas!");
@@ -46,7 +60,7 @@ function obterKpis(req, res) {
     }
 
     dashboardModel.obterKpis(id_loja, dataInicio, dataFim)
-        .then(function(resultado) {
+        .then(function (resultado) {
             let totalAtual = resultado[0][0].total_atual;
             let totalAnterior = resultado[1][0].total_anterior;
             let variacao = Math.round(((totalAtual - totalAnterior) / totalAnterior) * 100);
@@ -64,7 +78,7 @@ function obterKpis(req, res) {
                 tempoMedio: resultado[4][0].tempo_medio,
                 variacaoTempo: variacaoTempo
             });
-        }).catch(function(erro) {
+        }).catch(function (erro) {
             console.log(erro);
             res.status(500).json(erro.sqlMessage);
         });
