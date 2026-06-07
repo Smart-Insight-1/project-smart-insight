b3b940260c53b0b795aa5850775d194084511a7d
gicanalez <giovanna.canalez@sptech.school>
2026-06-07 10:36:24 -0300

    Resolvendo conflito do dashboard.js

--- src/models/dashboard.js
+++ src/models/dashboard.js
@@ -2,27 +2,27 @@ var database = require("../database/config")
 
 function obterInteracoesPorProduto(id_loja) {
     var instrucao = `
-        SELECT p.nome AS produto, COUNT(i.id_interacao) AS total_interacoes
-        FROM interacao i
-        JOIN sensor s ON i.id_sensor = s.id_sensor
-        JOIN produto p ON s.id_produto = p.id_produto
-        WHERE p.id_loja = ${id_loja}
-        GROUP BY p.nome
-        ORDER BY total_interacoes DESC
-        LIMIT 6;
+        SELECT
+            produto,
+            COUNT(*) AS total_interacoes
+        FROM vw_graficos
+            WHERE id_loja = '${id_loja}'
+            GROUP BY produto
+            ORDER BY total_interacoes DESC
+            LIMIT 6;
     `;
     return database.executar(instrucao);
 }
 
 function obterInteracoesPorSetor(id_loja) {
     var instrucao = `
-        SELECT sa.nome_setor AS setor, COUNT(i.id_interacao) AS total_interacoes
-        FROM interacao i
-        JOIN sensor s ON i.id_sensor = s.id_sensor
-        JOIN setor_amostra sa ON s.id_setor = sa.id_setor
-        WHERE sa.id_loja = ${id_loja}
-        GROUP BY sa.nome_setor
-        ORDER BY total_interacoes DESC;
+        SELECT
+            setor,
+            COUNT(*) AS total_interacoes
+        FROM vw_graficos
+            WHERE id_loja = '${id_loja}'
+            GROUP BY setor
+            ORDER BY total_interacoes DESC;
     `;
     return database.executar(instrucao);
 }
@@ -31,16 +31,14 @@ function obterInteracoesPorSetor(id_loja) {
 
 function obterTempoRetencao(id_loja) {
     var instrucao = `
-        SELECT 
-            p.nome AS produto, 
-            ROUND(AVG(i.duracao), 0) AS tempo_medio
-        FROM produto p
-        JOIN sensor s ON p.id_produto = s.id_produto
-        JOIN interacao i ON s.id_sensor = i.id_sensor
-        WHERE p.id_loja = ${id_loja}
-        GROUP BY p.id_produto, p.nome
-        ORDER BY tempo_medio DESC
-        LIMIT 6;
+        SELECT
+            produto,
+            ROUND(AVG(duracao),0) AS tempo_medio
+        FROM vw_graficos
+            WHERE id_loja = '${id_loja}'
+            GROUP BY produto
+            ORDER BY tempo_medio DESC
+            LIMIT 6;
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
@@ -112,15 +110,13 @@ AND DATE(i.horario) BETWEEN '${dataInicio}' AND '${dataFim}';
 
 function obterInteracoesPorHora(id_loja) {
     var instrucao = `
-        SELECT 
-            HOUR(i.horario) AS hora,
-            COUNT(i.id_interacao) AS total_interacoes
-        FROM interacao i
-        JOIN sensor s ON i.id_sensor = s.id_sensor
-        JOIN produto p ON s.id_produto = p.id_produto
-        WHERE p.id_loja = ${id_loja}
-        GROUP BY HOUR(i.horario)
-        ORDER BY hora ASC;
+        SELECT
+            HOUR(horario) AS hora,
+            COUNT(*) AS total_interacoes
+        FROM vw_graficos
+            WHERE id_loja = '${id_loja}'
+            GROUP BY HOUR(horario)
+            ORDER BY hora;
     `;
     return database.executar(instrucao);
 }
